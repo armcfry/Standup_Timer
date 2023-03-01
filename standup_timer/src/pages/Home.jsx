7ad97cf9d9955ecc89f4b0ff8f65d195fd823f13
Adam McFry <armcfry@gmail.com>
2023-02-28 21:48:04 -0700

    creating store/ redux; adding timer; organizing file structure

--- standup_timer/src/pages/Home.jsx
+++ standup_timer/src/pages/Home.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import Form from '../Form';
+import Form from '../components/Form';
 
 
 
-var standupVals = {duration:"", num_ppl:""};
-console.log(standupVals)
-const addStandup = (standupInfoVal) => {
-  //standupVals=standupInfoVal;
-  standupVals["duration"] = standupInfoVal["duration"]
-  standupVals["num_ppl"] = standupInfoVal["num_ppl"]
-  console.log(standupVals)
-};
+// var standupVals = {duration:"", num_ppl:""};
+// console.log(standupVals)
+// const addStandup = (standupInfoVal) => {
+//   //standupVals=standupInfoVal;
+//   standupVals["duration"] = standupInfoVal["duration"]
+//   standupVals["num_ppl"] = standupInfoVal["num_ppl"]
+//   console.log(standupVals)
+// };
 
 
 function Home() {
@@ -18,7 +18,7 @@ function Home() {
     <div>
       <header>
           <div>
-            <Form addStandup={addStandup}/>
+            <Form/>
           </div>
       </header>
     </div>
--- standup_timer/src/pages/Meeting.jsx
+++ standup_timer/src/pages/Meeting.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate} from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+import { resetValues } from '../store/valuesSlice';
+import CountdownTimer from '../components/CountdownTimer';
 
 function Meeting() {
     let navigate = useNavigate();
-    let location = useLocation();
+    const dispatch = useDispatch();
     const returnHome = () => {
+        dispatch(resetValues());
         navigate('/')
     };
+    // get the values from the valueStore
+    const { duration_value, attendees_value } = useSelector((state) => state.values);
+    // parse the time values from the duration
+    let [minutes, seconds] = duration_value.split(':').map(parseFloat);
+    // make default seconds to 0 if undefined (the user only inputs a whole number for the minutes)
+    if(seconds===undefined){
+        seconds = 0
+    }
+    
     return (
-        <div class="w3-container">
-            <h1>this is the page for meetings</h1>
-            <div>Duration: {location.state.duration}</div>
-            <div># attendees: {location.state.attendees}</div>
-            {/* display information from meeting object */}
+        <div className="w3-container">
+            <h1>Meeting:</h1>
+            <div>Duration: {duration_value}</div>
+            <div># attendees: {attendees_value}</div>
+            <div><CountdownTimer minutes={minutes} seconds={seconds}/></div>
             <button type="submit" onClick={returnHome}>Return Home</button>
         </div>
     );
